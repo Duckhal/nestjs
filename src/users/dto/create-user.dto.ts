1ea0e6d1cd25d0e46b2100bfab66746a6b9e2c3a
Duckhal <hoangduckhai1922@gmail.com>
2025-11-16 19:12:20 +0700

    crud and class-validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString, IsInt, Min } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsInt()
+  @Min(0)
   age: number;
 }
